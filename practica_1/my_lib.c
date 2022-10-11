bf75bd0a8d120a74b2df23bd782558387afe8602
Jordi Florit <jordifloritensenyat03@gmail.com>
2022-10-11 10:39:58 +0200

    my_strcpy commit

--- practica_1/my_lib.c
+++ practica_1/my_lib.c
@@ -22,6 +22,13 @@ void main(){
     }else{
         printf("Los stirngs son iguales\n");
     }
+
+    //test my_strcpy
+
+    str1 = *my_strcpy(str1, str2);
+    
+
+    //test my_strncpy
     
 
 }
@@ -40,6 +47,41 @@ int my_strcmp(const char *str1, const char *str2){
     }
 
     return *str1 - *str2;
+}
+
+char *my_strcpy(char *dest, const char *src){
+
+    int aux = 0;
+    while(src[aux] != '\0'){
+        dest[aux] = src[aux];
+        aux++;
+    }
+
+    return dest;
+}
+
+char *my_strncpy(char *dest, const char *src, size_t n){
+
+    int len;
+    for (len = 0; src[len] != '\0'; len++){
+        ;
+    }
+
+    if(len < n){
+        for(int i = 0; i < len; i++){
+            dest[i] = src[i];
+        }
+        for(int i = len; i < n; i++){
+            dest[i] = '\0';
+        }
+    }else{
+        for(int i = 0; i < n; i++){
+            dest[i] = src[i];
+        }
+    }
+    
+
+    return dest;
 }
 
 
