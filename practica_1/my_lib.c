86270837dbb416e9e6ce8dd4aaa3522678505305
pauxiron <pgrodriguez03@gmail.com>
2022-10-26 19:48:29 +0200

    write(3)

--- practica_1/my_lib.c
+++ practica_1/my_lib.c
@@ -209,25 +209,32 @@ void *my_stack_pop (struct my_stack *stack){
 }
 
 int my_stack_write(struct my_stack *stack, char *filename){
+
     int cont = 0;
+    int fd = 0;
     struct my_stack *stack_aux = my_stack_init(stack->size);
-    FILE *f = fopen(filename, "wb");
-    void *data;
-    while (stack_aux->top != NULL){
+    FILE *f = fopen(filename, "w");
+    
+    while ((stack_aux->top != NULL) && (cont != -1)){
         cont++;
-        fwrite(my_stack_pop(stack_aux),1,sizeof(stack_aux->top->data),f);
+        fd = fwrite(my_stack_pop(stack_aux),1,sizeof(stack_aux->top->data),f);
+        if (fd = open(filename,O_WRONLY) == -1){
+            cont = -1;
+        }
     }
+
     fclose(f);
     return cont;
+    
 }
 
 struct my_stack *my_stack_read(char *filename){
-    int fd;
 
+    int fd = 0;
     struct my_stack *stack_aux;
     FILE *f = fopen(filename, "rb");
-    //size_t  fread(void *p, size_t size, size_t n, FILE *pf)
-
+    
+    //size_t  fread(void *p, size_t size, size_t n, FILE *pf)    
     
 }
 
