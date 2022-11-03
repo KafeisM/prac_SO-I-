db857b5f90d7b803e42e168184c55ebfe44b7ef1
Jordi Florit <jordifloritensenyat03@gmail.com>
2022-11-03 12:21:47 +0100

    main

---
+++ practica_2/nivel1.c
@@ -0,0 +1,20 @@
+#define COMMAND_LINE_SIZE 1024
+#define PROMPT ‘$’
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+char *read_line(char *line); 
+
+int main(){
+    char line[COMMAND_LINE_SIZE];  
+    while(true){
+        if(read_line(line)){
+            execute_line(line);
+        }
+    }
+}
+
+
