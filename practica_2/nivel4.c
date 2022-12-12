bc31b97f4f24bf21978dd1c5183f9df68db7beb5
pauxiron <pgrodriguez03@gmail.com>
2022-12-12 21:55:50 +0100

    petit canvi

--- practica_2/nivel4.c
+++ practica_2/nivel4.c
@@ -212,8 +212,8 @@ int check_internal(char **args){
         return 1;
     }else if(strcmp(args[0],"exit")== 0){
         exit(0);
-    }else if(strcmp(args[0],"^C")){
-        return 0;
+    }else if(strcmp(args[0],"^C") == 0){
+        return 1;
     }else{ 
         printf("No es un comando interno\n");
         return 0;
