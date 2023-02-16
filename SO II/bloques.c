6340ef202b91a8639a71e6e7d8ec554b84e7ffaf
Jordi Florit <jordifloritensenyat03@gmail.com>
2023-02-16 13:59:37 +0100

    init

---
+++ SO II/bloques.c
@@ -0,0 +1,25 @@
+#include "bloques.h"
+
+static int descriptor = 0;
+
+int main(){
+    return 0;
+}
+
+int bmount(const char *camino){
+
+}
+
+int bumount(){
+
+}
+
+
+int bwrite(unsigned int nbloque, const void *buf){
+
+}
+
+
+int bread(unsigned int nbloque, void *buf){
+
+}
