91893fb6601ac518fa1a86dbef1af5f20ce54bb0
Jordi Florit <jordifloritensenyat03@gmail.com>
2023-02-18 00:25:47 +0100

    nivel 1 final

--- SO II/mi_mkfs.c
+++ SO II/mi_mkfs.c
@@ -7,37 +7,37 @@
 #include <string.h> // strerror()
 #include "bloques.h"
 
-static int descriptor = 0;
-
-
 //colores para errores
 #define RESET "\033[0m"
-#define NEGRO_T "\x1b[30m"
-#define NEGRO_F "\x1b[40m"
-#define GRIS_T "\x1b[94m"
 #define ROJO_T "\x1b[31m"
-#define VERDE_T "\x1b[32m"
-#define AMARILLO_T "\x1b[33m"
-#define AZUL_T "\x1b[34m"
-#define MAGENTA_T "\x1b[35m"
-#define CYAN_T "\x1b[36m"
-#define BLANCO_T "\x1b[97m"
 #define NEGRITA "\x1b[1m"
 
-int main(int argc, char **argv){
-    int num_blocks = atoi(argv[2]);
-    printf("%d\n",num_blocks);
+static int descriptor = 0;
 
-    if(argc != 3){
-        fprintf(stderr,"\nSintaxis correcta: ./mi_mkfs <nombre_dispositivo> <nbloques>");
-    }else{
-        // MONTAR EL DISPOSITIVO VIRTUAL
-       // descriptor = bmount(argv[1]);
-        // INICIAR FICHERO
-        
-        
-        
-        
+int main(int argc, char **argv){
+
+
+    if(argc -1 != 2){
+        fprintf(stderr,ROJO_T"Sintaxis correcta: ./mi_mkfs <nombre_dispositivo> <nbloques>\n"RESET);
+        exit(0);
     }
+
+    int num_blocks = atoi(argv[2]);
+    unsigned char buffer[1024];
+    memset(buffer, 0, sizeof(buffer));
+
+    // MONTAR EL DISPOSITIVO VIRTUAL
+    descriptor = bmount(argv[1]);
+    // INICIAR FICHERO
+    for (size_t i = 0; i < num_blocks; i++){
+        if(bwrite(i,&buffer) == -1){
+            fprintf(stderr,ROJO_T"ERROR INICIALIZACIÓN DE FICHERO\n"RESET);
+        }
+    }
+    //DESMONTAR FICHERO
+    if(bumount() == -1){
+        fprintf(stderr,ROJO_T"ERROR INICIALIZACIÓN DE FICHERO\n"RESET);
+    }
+        
     return 0;
 }
