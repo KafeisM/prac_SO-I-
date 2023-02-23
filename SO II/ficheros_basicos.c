af6116481a49baad11cb9dd1bfef0d698382b317
Jordi Florit <jordifloritensenyat03@gmail.com>
2023-02-23 13:02:41 +0100

    inicio nivel 2

---
+++ SO II/ficheros_basicos.c
@@ -0,0 +1,17 @@
+ #include "ficheros_basico.h"
+ 
+ int tamMB(unsigned int nbloques){
+    
+ }
+
+ int tamAI(unsigned int ninodos){
+
+ }
+
+ int initSB(unsigned int nbloques, unsigned int ninodos){
+
+ }
+
+ int initMB(){
+
+ }
