9ec6a61866a8fff1d84fa0a7cabac553d480e0b0
Jordi Florit <jordifloritensenyat03@gmail.com>
2023-03-02 11:02:27 +0100

    continuacio InitMB

--- SO II/ficheros_basico.c
+++ SO II/ficheros_basico.c
@@ -1,62 +1,6 @@
  #include "ficheros_basico.h"
 
-#define posSB 0 // el superbloque se escribe en el primer bloque de nuestro FS
-#define tamSB 1
-#define INODOSIZE 128 // tamaño en bytes de un inodo
-
-struct superbloque {
-   unsigned int posPrimerBloqueMB;          // Posición absoluta del primer bloque del mapa de bits
-   unsigned int posUltimoBloqueMB;          // Posición absoluta del último bloque del mapa de bits
-   unsigned int posPrimerBloqueAI;            // Posición absoluta del primer bloque del array de inodos
-   unsigned int posUltimoBloqueAI;            // Posición absoluta del último bloque del array de inodos
-   unsigned int posPrimerBloqueDatos;     // Posición absoluta del primer bloque de datos
-   unsigned int posUltimoBloqueDatos;     // Posición absoluta del último bloque de datos
-   unsigned int posInodoRaiz;                     // Posición del inodo del directorio raíz (relativa al AI)
-   unsigned int posPrimerInodoLibre;        // Posición del primer inodo libre (relativa al AI)
-   unsigned int cantBloquesLibres;            // Cantidad de bloques libres (en todo el disco)
-   unsigned int cantInodosLibres;              // Cantidad de inodos libres (en el AI)
-   unsigned int totBloques;                          // Cantidad total de bloques del disco
-   unsigned int totInodos;                            // Cantidad total de inodos (heurística)
-   char padding[BLOCKSIZE - 12 * sizeof(unsigned int)]; // Relleno para ocupar el bloque completo
-};
-
-struct inodo {     // comprobar que ocupa 128 bytes haciendo un sizeof(inodo)!!!
-   unsigned char tipo;     // Tipo ('l':libre, 'd':directorio o 'f':fichero)
-   unsigned char permisos; // Permisos (lectura y/o escritura y/o ejecución)
-
-
-   /* Por cuestiones internas de alineación de estructuras, si se está utilizando
-    un tamaño de palabra de 4 bytes (microprocesadores de 32 bits):
-   unsigned char reservado_alineacion1 [2];
-   en caso de que la palabra utilizada sea del tamaño de 8 bytes
-   (microprocesadores de 64 bits): unsigned char reservado_alineacion1 [6]; */
-   unsigned char reservado_alineacion1[6];
-
-
-   time_t atime; // Fecha y hora del último acceso a datos
-   time_t mtime; // Fecha y hora de la última modificación de datos
-   time_t ctime; // Fecha y hora de la última modificación del inodo
-
-
-   /* comprobar que el tamaño del tipo time_t para vuestra plataforma/compilador es 8:
-   printf ("sizeof time_t is: %ld\n", sizeof(time_t)); */
-
-
-   unsigned int nlinks;             // Cantidad de enlaces de entradas en directorio
-   unsigned int tamEnBytesLog;      // Tamaño en bytes lógicos (EOF)
-   unsigned int numBloquesOcupados; // Cantidad de bloques ocupados zona de datos
-
-
-   unsigned int punterosDirectos[12];  // 12 punteros a bloques directos
-   unsigned int punterosIndirectos[3]; /* 3 punteros a bloques indirectos:
-   1 indirecto simple, 1 indirecto doble, 1 indirecto triple */
-
-
-   /* Utilizar una variable de alineación si es necesario  para vuestra plataforma/compilador   */
-   char padding[INODOSIZE - 2 * sizeof(unsigned char) - 3 * sizeof(time_t) - 18 * sizeof(unsigned int) - 6 * sizeof(unsigned char)];
-   // Fijarse que también se resta lo que ocupen las variables de alineación utilizadas!!!
-};
-
+int binaryToDecimal(int byte[]);
 
 
 
@@ -135,8 +79,6 @@ struct inodo {     // comprobar que ocupa 128 bytes haciendo un sizeof(inodo)!!!
  int initMB(){    
     struct superbloque SB;
     bread(0,&SB);
-
-    
     char bufferMB[BLOCKSIZE];
     //numero de BITS del mapa (es el tamaño de SB + MB + AI), cada bit representa un bloque fisico
     int nbits = SB.posPrimerBloqueDatos;
@@ -145,25 +87,50 @@ struct inodo {     // comprobar que ocupa 128 bytes haciendo un sizeof(inodo)!!!
    if((nbits/8)/BLOCKSIZE == 0){
       int nbytes = nbits/8;
       
-
       for (int i = 0; i < nbytes; i++){
          bufferMB[i] = 255; //ponemos todos los bytes a 1 (elementos de la array)
       }
 
-      //calcular resto de bits que quedan
+      //calcular resto de bits que pueden quedar por meter
       int rest = nbits % 8;
       if(rest != 0){
-         bufferMB[nbytes] = 224; //QUEDA IMPLEMENTAR-LO
+
+         int byte[8];
+         for(int i = 0; i <rest; i++){
+            byte[i] = 1;
+         }
+         bufferMB[nbytes] = binaryToDecimal(byte);
+      }else{
+         bufferMB[nbytes] = 0;
       }
+
       
 
-   }else{
-
    }
+
 
     
 
  }
+
+ //funcion auxiliar para calcular el decimal de un numero binario
+ int binaryToDecimal(int byte[]) {
+    int decimal = 0;
+    int base = 1;
+    int i;
+    
+    // Recorre el array del byte en binario en orden inverso
+    for (i = 7; i >= 0; i--) {
+        // Si el bit actual es 1, suma su valor a decimal
+        if (byte[i] == 1) {
+            decimal += base;
+        }
+        // Actualiza la base multiplicando por 2 para pasar al siguiente bit
+        base *= 2;
+    }
+    
+    return decimal;
+}
 
 /*---------------------------------------------------------------------------------------------------------
 * Inicializa la lista de nodos libres
