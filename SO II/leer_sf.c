f6e4c52aaa9bd59674eb63f676e3a2df2f4915c1
Jordi Florit <jordifloritensenyat03@gmail.com>
2023-03-10 23:52:03 +0100

    leer_sf (fallo reservar bloque i datos inodo)

--- SO II/leer_sf.c
+++ SO II/leer_sf.c
@@ -2,6 +2,8 @@
 #include "bloques.h"
 
 void comprobarMB();
+void comprobarBloques();
+void comprobarInodo();
 
 const char *directorio;
 struct superbloque SB;
@@ -79,17 +81,62 @@ int main(int argc, char **argv){
 
     //comprobar reservar/liberar bloque
     printf("\nRESERVAMOS UN BLOQUE Y LUEGO LO LIBERAMOS\n");
-
+    comprobarBloques();
 
     //comprobar leer/escribir bit
     printf("\nMAPA DE BITS CON BLOQUES DE METADATOS OCUPADOS\n");
     comprobarMB();
+
+    //comrobar datos del directorio raiz
+    comprobarInodo();
+
     
     return EXITO;
 }
+
+
 
 void comprobarMB(){
     printf("posSB: %d -> leer_bit(%d) = %d\n",0,0,leer_bit(0));
     printf("SB.posPrimerBloqueMB: %d -> leer_bit(%d) = %d\n",SB.posPrimerBloqueMB,SB.posPrimerBloqueMB,leer_bit(SB.posPrimerBloqueMB));
     printf("SB.posUltimoBloqueMB: %d -> leer_bit(%d) = %d\n",SB.posUltimoBloqueMB,SB.posUltimoBloqueMB,leer_bit(SB.posUltimoBloqueMB));
+    printf("SB.posPrimerBloqueAI: %d -> leer_bit(%d) = %d\n",SB.posPrimerBloqueAI,SB.posPrimerBloqueAI,leer_bit(SB.posPrimerBloqueAI));
+    printf("SB.posUltimoBloqueAI: %d -> leer_bit(%d) = %d\n",SB.posUltimoBloqueAI,SB.posUltimoBloqueAI,leer_bit(SB.posUltimoBloqueAI));
+    printf("SB.posPrimerBloqueDatos: %d -> leer_bit(%d) = %d\n",SB.posPrimerBloqueDatos,SB.posPrimerBloqueDatos,leer_bit(SB.posPrimerBloqueDatos));
+    printf("SB.posUltimoBloqueDatos: %d -> leer_bit(%d) = %d\n",SB.posUltimoBloqueDatos,SB.posUltimoBloqueDatos,leer_bit(SB.posUltimoBloqueDatos));
+    
+}
+
+void comprobarBloques(){
+    printf("Se ha reservado el bloque físico no %d que era el 1er libre indicado por el MB\n",SB.posPrimerBloqueDatos);
+
+    //reservamos el primer bloque de datos
+    if(reservar_bloque(SB.posPrimerBloqueDatos) == FALLO){
+        fprintf(stderr,ROJO_T"FALLO AL RESERVAR BLOQUE\n"RESET);
+    }
+
+    printf("SB.cantBloquesLibres = %d\n",SB.cantBloquesLibres);
+
+    //liberamos ese mismo bloque
+    if(liberar_bloque(SB.posPrimerBloqueDatos) == FALLO){
+        fprintf(stderr,ROJO_T"FALLO AL LIBERAR BLOQUE\n");
+    }
+
+    printf("Liberamos ese bloque y después SB.cantBloquesLibres = %d\n",SB.cantBloquesLibres);
+
+}
+
+void comprobarInodo(){
+    struct inodo inodo;
+
+    if(leer_inodo(0,&inodo)){
+        fprintf(stderr,"ERROR EN LA LECTURA DEL INODO");
+    }
+
+    printf("\nDATOS DEL DIRECTORIO RAIZ\n");
+    printf("tipo: %c\n",inodo.tipo);
+    printf("permisos: %c\n",inodo.permisos);
+    printf("nlinks: %d\n",inodo.nlinks);
+    printf("tamEnBytesLog: %d\n",inodo.tamEnBytesLog);
+    printf("numBloquesOcupados: %d\n",inodo.numBloquesOcupados);
 }
