e1fff624f1b630faeb33b66005b6ea5d6fcbfbff
peepbii10 <pepbiel2003@gmail.com>
2023-03-20 22:08:41 +0100

    programa permitir.c

--- SO II/permitir.c
+++ SO II/permitir.c
@@ -1,5 +1,25 @@
 #include "ficheros.h"
 
-int main(){
-    
+//permitir <nombre_dispositivo> <ninodo> <permisos>
+int main(int argc, char **argv){
+
+    if((argv[1] == NULL) ||(argv[2] == NULL) || (argv[3] == NULL)){
+        return FALLO;
+    }else{
+        char *directorio = argv[1];
+        int ninodo = atoi(argv[2]);
+        int permisos = atoi(argv[3]);
+
+        if(bmount(directorio) == FALLO){
+            return FALLO;
+        }
+
+        mi_chmod_f(ninodo, permisos);
+
+        if(bumount(directorio) == FALLO){
+            return FALLO;
+        }
+    }
+
+    return EXITO;
 }
