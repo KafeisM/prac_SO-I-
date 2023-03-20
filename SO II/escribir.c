9b1205ff78a5378f2f9217377f37d16bbe6c6d75
peepbii10 <pepbiel2003@gmail.com>
2023-03-20 21:55:28 +0100

    funcions 3 i 4 de nivell 5

---
+++ SO II/escribir.c
@@ -0,0 +1,5 @@
+#include "ficheros.h"
+
+int main(){
+    
+}
--- SO II/ficheros.c
+++ SO II/ficheros.c
@@ -1,1 +1,48 @@
 #include "ficheros.h"
+
+int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offset, unsigned int nbytes){
+
+}
+
+int mi_read_f(unsigned int ninodo, void *buf_original, unsigned int offset, unsigned int nbytes){
+
+}
+
+int mi_stat_f(unsigned int ninodo, struct STAT *p_stat){
+
+    struct inodo inodos;
+
+    if(leer_inodo(ninodo, &inodos) == FALLO){
+        return FALLO;
+    }
+
+    p_stat->atime = inodos.atime;
+    p_stat->ctime = inodos.ctime;
+    p_stat->mtime = inodos.mtime;
+    p_stat->nlinks = inodos.nlinks;
+    p_stat->permisos = inodos.permisos;
+    p_stat->tipo = inodos.tipo;
+    p_stat->tamEnBytesLog = inodos.tamEnBytesLog;
+    p_stat->numBloquesOcupados = inodos.numBloquesOcupados;
+
+    return EXITO;
+
+}
+
+int mi_chmod_f(unsigned int ninodo, unsigned char permisos){
+    
+    struct inodo inodos;
+
+    if(leer_inodo(ninodo, &inodos) == FALLO){
+        return FALLO;
+    }
+
+    inodos.permisos = permisos; //Cambiamos permisos
+    inodos.ctime = time(NULL); //Reseteamos el ctime porque hemos modificado el inodo
+
+    if(escribir_inodo(ninodo, &inodos) == FALLO){
+        return FALLO;
+    }
+
+    return EXITO;
+}
