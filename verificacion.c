df73436399704d7c6e925f6e07aeea9155f5f11c
peepbii10 <pepbiel2003@gmail.com>
2023-05-23 19:33:07 +0200

    verificacion.c

--- verificacion.c
+++ verificacion.c
@@ -1,3 +1,12 @@
 /*JOSEP GABRIEL FORNÉS REYNÉS, JORDI FLORIT ENSENYAT, PAU GIRÓN RODRÍGUEZ*/
 #include "verificacion.h"
 
+int main(int argc, char **argv){
+
+    if (argc != 3) {
+      fprintf(stderr, ROJO_T"Sintaxis: ./verificacion <nombre_dispositivo> <directorio_simulación>\n" RESET);
+      exit(FALLO);
+   }
+
+}
+
