54d11589eb4d8983e11769e5f0d586457f442227
peepbii10 <pepbiel2003@gmail.com>
2023-05-23 19:33:44 +0200

    verificacion.c

--- verificacion.c
+++ verificacion.c
@@ -3,10 +3,25 @@
 
 int main(int argc, char **argv){
 
+    struct STAT stat;
+    int numeroEntradas = 0;
+
     if (argc != 3) {
       fprintf(stderr, ROJO_T"Sintaxis: ./verificacion <nombre_dispositivo> <directorio_simulación>\n" RESET);
       exit(FALLO);
-   }
+    }
+
+    if(bmount(argv[1]) == FALLO){
+        fprintf(stderr, ROJO_T"Error montar dispositivo"RESET);
+        return FALLO;
+    }
+
+    mi_stat(argv[2], &stat);
+
+    numeroEntradas = (stat.tamEnBytesLog / sizeof(struct entrada)); //obtenemos numero de entradas
+    
+    fprintf("Numero de entradas: %d", numeroEntradas);
+
 
 }
 
